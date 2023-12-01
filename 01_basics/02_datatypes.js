1a3b72ed2f32a776256a16ea3db0d3cdd04464e5
PrafulAswale <66135235+PrafulAswale@users.noreply.github.com>
2023-12-01 10:47:09 +0000

    datatype

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,14 @@
+"use strict";// treats all JS code as newer Version
+
+//number  => 2 to the power 53
+//bigInt 
+// string =>""
+//boolean => true/false
+// null => standalone value
+//undefined => 
+// //symbol => unique 
+
+//object
+
+console.log(typeof undefined);
+console.log(typeof null);
